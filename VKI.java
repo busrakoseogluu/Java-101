7440967f491f92eace9de022e8cd2f5ad21db932
busrakoseogluu <104007528+busrakoseogluu@users.noreply.github.com>
2022-04-22 14:47:38 +0300

    Update VKI.java

--- VKI.java
+++ VKI.java
@@ -5,9 +5,9 @@ public class VKI {
 	public static void main(String[] args) {
 
 		/*
-		 * Java ile kullanıcıdan boy ve kilo değerlerini alıp bir değişkene atayın.
-		 * Aşağıdaki formüle göre kullanıcının "Vücut Kitle İndeks" değerini hesaplayıp
-		 * ekrana yazdırın. Formül Kilo (kg) / Boy(m) * Boy(m)
+		 * Java ile kullanÄ±cÄ±dan boy ve kilo deÄŸerlerini alÄ±p bir deÄŸiÅŸkene atayÄ±n.
+		 * AÅŸaÄŸÄ±daki formÃ¼le gÃ¶re kullanÄ±cÄ±nÄ±n "VÃ¼cut Kitle Ä°ndeks" deÄŸerini hesaplayÄ±p
+		 * ekrana yazdÄ±rÄ±n. FormÃ¼l Kilo (kg) / Boy(m) * Boy(m)
 		 */
 
 		Scanner scan = new Scanner(System.in);
@@ -17,10 +17,10 @@ public class VKI {
 		System.out.print("Boyunuzu m cinsinden giriniz : ");
 		boy=scan.nextDouble();
 		System.out.print("Kilonuzu giriniz : ");
-        kilo=scan.nextDouble();
+                kilo=scan.nextDouble();
         
-        vki=kilo/(boy*boy);
-        System.out.println("Vücut kitle indeksiniz : " + vki);
+                vki=kilo/(boy*boy);
+                System.out.println("VÃ¼cut kitle indeksiniz : " + vki);
 		
 	}
 
