02a2a9b63fa0d4e97d50919a89e6d5f033b7c745
busrakoseogluu <104007528+busrakoseogluu@users.noreply.github.com>
2022-04-23 15:49:28 +0300

    Add files via upload

---
+++ etkinlik.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class etkinlik {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Hava sýcaklýðýný giriniz : ");
+		int hava = scan.nextInt();
+
+		if (hava < 5) {
+			System.out.println("Kayak yapabilirsiniz.");
+		} else if (hava < 15 && hava >= 5) {
+			System.out.println("Sinemaya gidebilirsiniz.");
+		} else if (hava >= 15 && hava < 25) {
+			System.out.println("Piknik yapabilirsiniz.");
+		} else {
+			System.out.println("Yüzmeye gidebilirsiniz");
+		}
+
+	}
+
+}
