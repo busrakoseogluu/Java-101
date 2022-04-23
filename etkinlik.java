93c45189823064fe35356a2f31355f7dc14809f7
busrakoseogluu <104007528+busrakoseogluu@users.noreply.github.com>
2022-04-23 15:50:03 +0300

    Update etkinlik.java

--- etkinlik.java
+++ etkinlik.java
@@ -5,7 +5,7 @@ public class etkinlik {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
-		System.out.print("Hava sıcaklığını giriniz : ");
+		System.out.print("Hava sÄ±caklÄ±ÄŸÄ±nÄ± giriniz : ");
 		int hava = scan.nextInt();
 
 		if (hava < 5) {
@@ -15,7 +15,7 @@ public class etkinlik {
 		} else if (hava >= 15 && hava < 25) {
 			System.out.println("Piknik yapabilirsiniz.");
 		} else {
-			System.out.println("Yüzmeye gidebilirsiniz");
+			System.out.println("YÃ¼zmeye gidebilirsiniz");
 		}
 
 	}
