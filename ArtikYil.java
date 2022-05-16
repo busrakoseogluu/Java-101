cfb40e46880d22c05592fc67fd627fdb5b9774e2
busrakoseogluu <104007528+busrakoseogluu@users.noreply.github.com>
2022-05-16 17:18:57 +0300

    Add files via upload

---
+++ ArtikYil.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ArtikYil {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Yýl giriniz : ");
+		int yil=scan.nextInt();
+		
+		if(yil%4==0) {
+			if(yil%100==0) {
+				if(yil%400==0) {
+					System.out.println(yil + " bir artýk yýldýr.");
+				}
+			}
+			System.out.println(yil + " bir artýk yýldýr.");
+		}else {
+			System.out.println(yil + " bir artýk yýl deðildir.");
+		}
+		
+	}
+
+}
