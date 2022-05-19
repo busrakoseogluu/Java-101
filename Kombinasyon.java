8bfd5f1958179fe2215efe7a66254c20c44ad9de
busrakoseogluu <104007528+busrakoseogluu@users.noreply.github.com>
2022-05-19 18:47:56 +0300

    Add files via upload

---
+++ Kombinasyon.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Kombinasyon {
+
+	public static void main(String[] args) {
+		/*
+		 * C(n,r) = n! / (r! * (n-r)!)
+		 */
+		Scanner scan = new Scanner(System.in);
+		int f = 1,f1=1,f2=1;
+		System.out.print("Eleman Sayýsý (n) : ");
+		int n = scan.nextInt();
+		System.out.print("Seçim Sayýsý (r) : ");
+		int r = scan.nextInt();
+
+		if((n>0 && r>0) && n>=r) {
+		for (int i = 1; i <= n; i++) {
+			f = f * i;
+		}
+		for(int i=1;i<=r;i++) {
+			f1=f1*i;
+		}
+		for (int i = 1; i <= n-r; i++) {
+			f2=f2*i;
+		}
+		int factorial=f/(f1*f2);
+		System.out.println(factorial);
+		}else {
+			System.out.println("n r'den büyük olmalý!");
+		}
+	}
+}
