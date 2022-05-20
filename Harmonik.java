9e2ef62dd6f6af4c50be972372bb00ab8c77f612
busrakoseogluu <104007528+busrakoseogluu@users.noreply.github.com>
2022-05-20 13:36:29 +0300

    Add files via upload

---
+++ Harmonik.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Harmonik {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Sayý adedini giriniz : ");
+		int n = scan.nextInt();
+		double result = 0;
+		for (int i = 1; i <= n; i++) {
+			result += (1.0 / i);
+		}
+		System.out.println("Sonuç : " + result);
+	}
+
+}
