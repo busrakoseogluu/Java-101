adce7cb194d8e5e84ff547032d1c3d0334047b33
busrakoseogluu <104007528+busrakoseogluu@users.noreply.github.com>
2022-05-20 14:11:03 +0300

    Add files via upload

---
+++ UcgenYapimi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class UcgenYapimi {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Basamak sayýsýný giriniz : ");
+		int n = scan.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			for (int k = 1; k <= (n - i); k++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= (2 * i) - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
