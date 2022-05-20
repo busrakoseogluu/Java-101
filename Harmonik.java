e8c101003939651dc6d9072fb3fe0836be663165
busrakoseogluu <104007528+busrakoseogluu@users.noreply.github.com>
2022-05-20 13:36:53 +0300

    Update Harmonik.java

--- Harmonik.java
+++ Harmonik.java
@@ -5,13 +5,13 @@ public class Harmonik {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Sayý adedini giriniz : ");
+		System.out.print("SayÄ± adedini giriniz : ");
 		int n = scan.nextInt();
 		double result = 0;
 		for (int i = 1; i <= n; i++) {
 			result += (1.0 / i);
 		}
-		System.out.println("Sonuç : " + result);
+		System.out.println("SonuÃ§ : " + result);
 	}
 
 }
